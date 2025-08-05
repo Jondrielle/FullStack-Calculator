561208a3133d6b16829f41766d25686dcf4ef70a
Jondri <jondriellewilson@gmail.com>
2025-08-04 23:23:35 -0500

    Restructured project and added arithmetic test functions all tests pass

---
+++ test_calculator_logic.py
@@ -0,0 +1,19 @@
+from backend.logic.calculator import add, sub, multiply, division
+
+def test_add():
+    assert add(4,3) == 7
+
+def test_sub():
+    assert sub(3,-12321) == 12324
+
+def test_multiply():
+    assert multiply(2,1) == 2
+
+def test_division():
+    result = division(3,124) 
+    assert round(result,4) == 0.0242
+
+def test_division_by_zero():
+    result = division(4, 0)
+    assert isinstance(result, str)  # it should return an error message string
+    assert "error" in result.lower()  # the message contains "error"
